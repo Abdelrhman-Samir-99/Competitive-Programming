10b371bb6a125dad3e28f6a6880b724ab7ce42e5
Abdelrhman Samir <AbdelrhmanSamir3699@gmail.com>
2021-01-14 18:57:01 +0200

    Completing BST.

--- Graphs/Trees/Binary Search Tree/BST.cpp
+++ Graphs/Trees/Binary Search Tree/BST.cpp
@@ -76,6 +76,70 @@ class BST {
 			return do_upper_bound(key, current_node-> right);
 		}
 
+		void do_erase(int key, node* current_node, node* my_parent, bool is_Left = false) {
+
+			// is_Left -> to keep track if the current_node is left or right to the parent.
+			// we maintain my_parent to link the nodes after removal.
+
+			// this key do not exist.
+			if(current_node == NULL)
+				return;
+
+			// if my left child is the same as me then i just need to delete this node
+			if(current_node-> left != NULL && current_node-> left -> key == key) {
+				if(is_Left == true) {
+					my_parent-> left = current_node-> left;
+				}
+				else {
+					my_parent-> right = current_node-> left;
+				}
+				delete current_node;
+				return;
+			}
+			
+			if(current_node-> key == key) {
+				if(current_node-> left == NULL && current_node-> right == NULL) {
+					// this node is a leaf.
+					delete current_node;
+				}
+				else if(current_node-> left != NULL && current_node-> right == NULL) {
+					// only left child.
+					if(is_Left == true) {
+						my_parent-> left = current_node-> left;
+					}
+					else {
+						my_parent-> right = current_node-> left;
+					}
+					delete current_node;
+				}
+				else if(current_node-> left == NULL && current_node-> right != NULL) {
+					// only right child.
+					if(is_Left == true) {
+						my_parent-> left = current_node-> right;
+					}
+					else {
+						my_parent-> right = current_node-> right;
+					}
+					delete current_node;
+				}
+				else {
+					// have 2 childs.
+					// get the lower_bound node (there must be one since there's a right child).
+					node* best_value = this-> lower_bound(current_node-> key);
+					current_node-> key = best_value-> key;
+					erase(current_node-> key);
+					delete current_node;
+				}
+				return;
+			}
+			if(key > current_node-> key) {
+				do_erase(key, current_node-> right, current_node, false);
+			}
+			else {
+				do_erase(key, current_node-> left, current_node, true);
+			}
+		}
+
 		
 	public:
 	
@@ -128,6 +192,7 @@ class BST {
 
 		void erase(int key) {
 			assert(root != NULL);
+			do_erase(key, root, NULL);
 		}
 
 		void clear() {
